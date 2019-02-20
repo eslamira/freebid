507cb99b730b428043e3deb4d60e2308ab7e3ea3
zer0a <33692485+zer0a@users.noreply.github.com>
2019-02-21 01:17:38 +0200

    isloggenin

--- lib/UI/splash_screen.dart
+++ lib/UI/splash_screen.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:freebid/UI/Utils/auth_client.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -7,35 +10,50 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  final AuthClient _authClient = AuthClient.internal();
+
   @override
   void initState() {
     super.initState();
     time();
+    _isLoggedIn();
+  }
+
+  Future<FirebaseUser> _isLoggedIn() async {
+    return await _authClient.isLoggedIn();
   }
 
   void time() async {
     Timer(Duration(seconds: 3), () async {
-      Navigator.of(context).pushReplacementNamed('/login');
+      _isLoggedIn() == null
+          ? Navigator.of(context).pushReplacementNamed('/login')
+          : Navigator.of(context).pushReplacementNamed('/categories');
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child: Image.asset('assets/images/background.png',fit: BoxFit.fill,),
+        body: Stack(
+      children: <Widget>[
+        Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Image.asset(
+            'assets/images/background.png',
+            fit: BoxFit.fill,
           ),
-          Container(
-            alignment: Alignment.topCenter,
-            margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top + 230),
-            child: Image.asset('assets/images/Freebid.png',fit: BoxFit.fill,),
+        ),
+        Container(
+          alignment: Alignment.topCenter,
+          margin:
+              EdgeInsets.only(top: MediaQuery.of(context).padding.top + 230),
+          child: Image.asset(
+            'assets/images/Freebid.png',
+            fit: BoxFit.fill,
           ),
-        ],
-      )
-    );
+        ),
+      ],
+    ));
   }
 }
